2d47dfeeab84c7efbfb880534747c4695831e8bc
AmaraABC <amara-noah.gakou@efrei.net>
2025-09-27 22:27:40 +0200

    Updated project (functionalities, structure and style)

--- quotes/main.js
+++ quotes/main.js
@@ -7,12 +7,41 @@ function fetchQuote() {
             return res.json();
         })
         .then(data => {
-            document.querySelector(".quote p").innerHTML = `${data.value}`
+            document.querySelector(".quote p").innerHTML = `${data.value}`;
+            document.querySelector(".favorite-quote-btn").style.display = "block";
         })
         .catch(error => {
             console.error('Erreur:', error);
+            document.querySelector(".quote p").innerHTML = "Unable to display a quotation";
+            document.querySelector(".quote p").style.color = "rgb(255, 60, 60)";
         });
 };
 
-const favoriteQuotes = document.createElement("article");
-favoriteQuotes.setAttribute("class", "favorite-quote");
+function addFavoriteQuote() {
+    const quote = document.querySelector(".quote p").innerHTML;
+    let favorites = JSON.parse(localStorage.getItem("favorite-quote")) || [];
+
+    if (!favorites.includes(quote)) {
+        favorites.push(quote);
+        localStorage.setItem('favorite-quote', JSON.stringify(favorites));
+        displayFavoriteQuotes();
+    };
+};
+
+function displayFavoriteQuotes() {
+    const quotesSection = document.querySelector(".favorite-quotes-block");
+    let favorites = JSON.parse(localStorage.getItem("favorite-quote")) || [];
+
+    quotesSection.innerHTML = "";
+
+    favorites.forEach((quote) => {
+        const div = document.createElement('div');
+        const p = document.createElement('p');
+        div.classList.add('favorite-quote');
+        p.innerHTML = quote;
+        div.appendChild(p);
+        quotesSection.appendChild(div)
+    });
+};
+
+displayFavoriteQuotes();
