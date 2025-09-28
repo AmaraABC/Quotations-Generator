c8852381bfc235d40e52450dc19c50be0e6c8be1
AmaraABC <amara-noah.gakou@efrei.net>
2025-09-28 08:09:14 +0200

    Updated project (functionalities, structure and style) #2

--- quotes/main.js
+++ quotes/main.js
@@ -8,7 +8,7 @@ function fetchQuote() {
         })
         .then(data => {
             document.querySelector(".quote p").innerHTML = `${data.value}`;
-            document.querySelector(".favorite-quote-btn").style.display = "block";
+            document.querySelector(".favorite-quote-btn").style.display = "inline-block";
         })
         .catch(error => {
             console.error('Erreur:', error);
@@ -24,8 +24,19 @@ function addFavoriteQuote() {
     if (!favorites.includes(quote)) {
         favorites.push(quote);
         localStorage.setItem('favorite-quote', JSON.stringify(favorites));
+        alert("Citation ajoutée aux favoris !");
         displayFavoriteQuotes();
+    } else {
+        document.querySelector(".favorite-quote-btn").setAttribute("disabled", true);
     };
+};
+
+function deleteFavoriteQuote(index) {
+    let favorites = JSON.parse(localStorage.getItem("favorite-quote")) || [];
+    favorites.splice(index, 1);
+    localStorage.setItem('favorite-quote', JSON.stringify(favorites));
+    alert("Citation totalement supprimée !");
+    displayFavoriteQuotes();
 };
 
 function displayFavoriteQuotes() {
@@ -34,13 +45,32 @@ function displayFavoriteQuotes() {
 
     quotesSection.innerHTML = "";
 
-    favorites.forEach((quote) => {
+    favorites.forEach((quote, index) => {
         const div = document.createElement('div');
-        const p = document.createElement('p');
         div.classList.add('favorite-quote');
+        div.style.backgroundColor = "gold";
+        div.style.padding = "1.25em";
+        div.style.marginBlock = "1rem";
+        div.style.borderRadius = ".75rem";
+        div.style.position = "relative";
+
+        const p = document.createElement('p');
         p.innerHTML = quote;
+        p.style.fontSize = "clamp(.75rem, 1.4vw, 1rem)";
+
+        const button = document.createElement('button');
+        button.classList.add("delete-btn");
+        button.innerHTML = "Supprimer ?";
+        button.addEventListener("click", () => {
+            const confirmSupression = confirm("Voulez-vous vraiment supprimer cette citation ?!");
+            if (confirmSupression) {
+                deleteFavoriteQuote(index);
+            };
+        });
+
         div.appendChild(p);
-        quotesSection.appendChild(div)
+        div.appendChild(button);
+        quotesSection.appendChild(div);
     });
 };
 
